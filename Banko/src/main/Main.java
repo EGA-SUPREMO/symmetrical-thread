d20df831bc90d20b2ab705929097b8ec7be28d18
Ale <cxiesestro@gmail.com>
2020-01-08 09:36:09 -0400

    clean the code and another experiment

--- Banko/src/main/Main.java
+++ Banko/src/main/Main.java
@@ -1,5 +1,6 @@
-package malnovaĵejoj1;
+package main;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,15 +12,15 @@ public class Main {
 		/*while(b.geti()!=1900000)
 			b.mon_transiro((byte) (Math.random()*127), (byte) (Math.random()*127));*/
 		
-		for(int i = 0; i<2; i++) {
+		for(byte i = 0; i<127; i++) {
 			
-			Thread h = new Thread(new mon_transiro1(b));
+			Thread h = new Thread(new mon_transiro1(b, i));
 			
 			h.start();
 			//System.out.println(b.geti());
 			try {
 				
-				if(i==1)
+				if(i==126)
 					h.join();
 				
 			} catch (InterruptedException e) {
@@ -36,18 +37,26 @@ public class Main {
 
 class Banko {
 	
-	private final double kontj[] = new double[127];
-	private int i = 0;
-	private Lock l = new ReentrantLock();
+	private final double kontj[];
+	private int i;
+	//private byte iii;
+	private Lock l;
+	private Condition suficxmon;
 	
 	public Banko() {
+		
+		l = new ReentrantLock();
+		suficxmon = l.newCondition();
+		kontj = new double[127];
+		i = 0;
+		//iii = 0;
 		
 		for(int i = 0; i<127;i++)
 			kontj[i] = 3000;
 		
 	}
-	
-	void mon_transiro(byte devenaK, byte destinaK, mon_transiro1 h) {
+	//Ankaŭ vi povas uzi la vorton "synchronized"
+	void mon_transiro(byte devenaK, byte destinaK, int h) {
 		
 		/*if(devenaK == destinaK)
 			return;*/
@@ -56,31 +65,74 @@ class Banko {
 		
 		try {
 			
-			double kvanto = Math.round(Math.random() * kontj[devenaK]);
+			//double kvanto = Math.round(Math.random() * 3000);
+			double kvanto = (int) (Math.random() * 1000);
+			
+			while(kontj[devenaK]<kvanto)
+				suficxmon.await();
 			
 			kontj[devenaK] -= kvanto;
 			kontj[destinaK] += kvanto;
 			i++;
+			//iii++;
+			suficxmon.signalAll();
 			
-			/*System.out.println("La konto " + (devenaK + 1) + " transdonis la monon al la konto " + (destinaK + 1) +
+			System.out.println("La konto " + (devenaK + 1) + " transdonis la monon al la konto " + (destinaK + 1) +
 					", la nombro estas " + kvanto + " bolivaroj.\nLa tuto estas: " + getTut() +
-					", \u011Di estas la " + (h.gethi() + 1) + "-a transdonado de " + Thread.currentThread().getName() +
-					" kaj la " + i + "-a transdonado");*/
-			
-			if(kontj[devenaK]==0) {
+					", \u011Di estas la " + (h + 1) + "-a transdonado de " + Thread.currentThread().getName() +
+					" kaj la " + i + "-a transdonado");
+			if(isCXiuThreadsWaiting()) {
 				
 				imprimikontn();
-				System.out.println("Bonege! la " + (devenaK + 1) + "-a konto jam estas malplenri\u0109a, Feli\u0109on al vi kaj la viaj por ke \u0109io estu la\u016D via deziro!");
-				System.exit(0);
 				
 			}
+			/*if(iii==50) {
+				
+				iii=0;
+				Thread.dumpStack();Thread.currentThread().getThreadGroup().list();
+				for(byte b = 0; b<100;b++) {
+					
+					//System.out.println();
+					System.out.println(Thread.currentThread().getThreadGroup());
+					
+					
+				}
+			}*/
+			/*if(kontj[devenaK]==0) {
+				
+				imprimikontn();
+				System.out.println("Bonege! la " + (devenaK + 1) +
+				"-a konto jam estas malplenri\u0109a, Feli\u0109on al vi kaj la viaj por ke \u0109io estu la\u016D via deziro!");
+				System.exit(0);
+				
+			}*/
 			
+		}catch(InterruptedException e) {
+			e.printStackTrace();
 		}finally {
 			
 			l.unlock();
 			
 		}
 		
+	}
+	
+	boolean isCXiuThreadsWaiting() {
+		
+		Thread j[] = new Thread[127];
+		Thread.currentThread().getThreadGroup().enumerate(j);
+		byte b = 0;
+		byte c = 0;
+		
+		for(Thread g:j) {
+			try {
+				if(g.getState().toString().equals("WAITING"))
+					b++;
+			}catch(NullPointerException e) {
+				c--;
+			}
+		}
+		return b == (c - 1);
 	}
 	
 	double getTut() {
@@ -109,7 +161,7 @@ class Banko {
 	public void setKontj(byte i, double h) {
 		kontj[i] = h;
 	}
-
+	
 	void imprimikontn() {
 		
 		byte g[] = new byte[26];
@@ -149,7 +201,7 @@ class Banko {
 					g[ij] = ii;
 					ij++;
 					
-			}
+				}
 		
 		}
 		
@@ -192,16 +244,19 @@ class mon_transiro1 implements Runnable {
 	private static Banko b;
 	private int hi = 0;
 	
-	public mon_transiro1(Banko bb) {
+	public mon_transiro1(Banko bb, byte dk) {
+		
 		b = bb;
+		devenaK = dk;
+		
 	}
 	
 	@Override
 	public void run() {
-		
-		for(; hi<30000000 ; hi++) {
+		while(true) {
+		//for(; hi<200; hi++) {
 			
-			devenaK = (byte) (Math.random()*127);
+			//devenaK = (byte) (Math.random()*127);
 			
 			do
 				destinaK = (byte) (Math.random()*127);
@@ -217,14 +272,11 @@ class mon_transiro1 implements Runnable {
 					", la nombro estas " + kvanto + " bolivaroj.\nLa tuto estas: " + b.getTut() + ", \u011Di estas la " + b.geti() +
 					"-a transdonado.\nFarita de: " + Thread.currentThread().getName());*/
 			
-			b.mon_transiro(devenaK, destinaK, this);
+			//b.mon_transiro(devenaK, destinaK, this);
+			b.mon_transiro(devenaK, destinaK, hi);
 			
 		}
 		
-	}
-	
-	int gethi() {
-		return hi;
 	}
 	
 }
